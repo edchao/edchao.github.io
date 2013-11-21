700cf5c720af206c0c1b46b19ea2242d9f672178
Ed Chao <calibercross@gmail.com>
2013-11-20 20:54:09 -0800

    mobile sidebar

--- port/js/app.js
+++ port/js/app.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
 
 
+
+
+
+
+
+
 menuSize();
+
+
 
 
 $(window).on('resize', function(){
@@ -13,28 +21,40 @@ $(window).on('scroll', function(){
 });
 
 
+$('.compact').on('click',function(){
+	var $containerMarginPx = $('.container').css('margin-left');
+	var containerMargin = parseInt($containerMarginPx, 10);
+
+	if(containerMargin < 5){
+		$('.container').css({'margin-left' : '100px'});
+	}else{
+		$('.container').css({'margin-left' : '0px'});
+	}
+});	
+
+$('.container').on('click',function(){
+	var $containerMarginPx = $('.container').css('margin-left');
+	var containerMargin = parseInt($containerMarginPx, 10);
+	if(containerMargin > 0){
+		$('.container').css({'margin-left' : '0px'});
+	}
+});	
 
 function menuSize(){
 	$viewWidth = $(document).width();
 	if ($viewWidth < 400){
 		$('nav').css({'-webkit-justify-content' : 'flex-start'});
 		$('nav').css({'justify-content' : 'flex-start'});
+		$('.expanded').css({'display':'none'});
+		$('.compact').css({'display':'block'});
 
-		$('nav').html(
-			'<div class="compact">\
-				&nbsp\
-			</div>'
-		)
+
 	}else {
 		$('nav').css({'-webkit-justify-content' : 'center'});
 		$('nav').css({'justify-content' : 'center'});
-		$('nav').html(
-			'<ul>\
-  			<a href=""><li>WORK</li></a>\
-     		 <a href=""><li>ABOUT</li></a>\
-     		 <a href=""><li>STREAM</li></a>\
-  			</ul>'
-		)	
+		$('.expanded').css({'display':'block'});
+		$('.compact').css({'display':'none'});
+		$('.container').css({'margin-left' : '0px'});
 	}
 }
 
@@ -44,16 +64,16 @@ function menuAnimate(){
 
 $yPos = $(document).scrollTop();
 
-if($yPos > 5){
-	$('nav').css({'background-color' : 'rgba(255,255,255,0.9)'});
-	$('nav li').css({'border-color':'rgba(0,0,0,0.0'});
-}else{
-	$('nav').css({'background-color' : 'transparent'});
-	$('nav li').css({'border-color':'rgba(0,0,0,0.3'});
-
+	if($yPos > 5){
+		$('nav').css({'background-color' : 'rgba(255,255,255,0.9)'});
+		$('nav li').css({'border-color':'rgba(0,0,0,0.0'});
+	}else{
+		$('nav').css({'background-color' : 'transparent'});
+		$('nav li').css({'border-color':'rgba(0,0,0,0.3'});
+	}
 }
 
 
-}
+
 
 });
