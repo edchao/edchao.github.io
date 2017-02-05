9c576ad83de122d8d1dcb20cb194052e70111efc
Ed Chao <eddchao@gmail.com>
2017-02-05 15:30:11 -0800

    email bit

--- js/app.js
+++ js/app.js
@@ -1,64 +1,43 @@
 $(document).ready(function(){
-
-
-// $(window).on('scroll', function(){
-//   menuAnimate();
-// });
-
-
-$('.contact').on('click', function(){
   var oink = 'eddchao';
-  window.location = "mailto:"+oink+"@gmail.com";
-});
-
-$('.link_email').on('click', function(){
-  var oink = 'eddchao';
-  window.location = "mailto:"+oink+"@gmail.com";
-});
 
 
-gridAnimate();
+  gridAnimate();
 
-/* Every time the window is scrolled ... */
-$(window).scroll( function(){
+  /* Every time the window is scrolled ... */
+  $(window).scroll( function(){
 
-    gridAnimate();
-
-});
-
-
-function gridAnimate(){
-  $('.hide_me').each( function(i){
-
-      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-      var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-      /* If the object is completely visible in the window, fade it it */
-      if( bottom_of_window + 500 > bottom_of_object ){
-
-          $(this).animate({'opacity':'1'},500);
-
-      }
+      gridAnimate();
 
   });
-}
 
 
-// function menuAnimate(){
-//
-//   $yPos = $(document).scrollTop();
-//
-//   $contentPos = $('.nav-trigger').offset().top;
-//
-//   if($yPos > $contentPos){
-//     $('nav').addClass('opaque');
-//     $('.nav_logo').addClass('is-shown');
-//   }else{
-//     $('nav').removeClass('opaque');
-//     $('.nav_logo').removeClass('is-shown');
-//   }
-// }
+  function gridAnimate(){
+    $('.hide_me').each( function(i){
 
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        /* If the object is completely visible in the window, fade it it */
+        if( bottom_of_window + 500 > bottom_of_object ){
+
+            $(this).animate({'opacity':'1'},500);
+
+        }
+
+    });
+  }
+
+
+  $('.contact').on('click', function(){
+    $(".modal_overlay").addClass("show");
+    $(".modal_message").html(oink+"@gmail.com")
+  });
+
+  $(".modal_dismiss").click(function(){
+    $(".modal_message").html("hi bots");
+    $(".modal_overlay").removeClass("show");
+  });
 
 
 
